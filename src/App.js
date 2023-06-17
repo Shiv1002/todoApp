479b4ca183f9eee2dc55db3518115bf92de58855
shivank <shivank.pasi@somaiya.edu>
2023-06-17 17:30:45 +0530

    chore:all funtion added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import {  useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState([{ title: 'kl', isComplete: false }])
+  const [task, setTask] = useState('')
+  const [showConfirm, setShowConfirm] = useState(false)
+  const [todoDelete, setTodoDelete] = useState(null)
+  const userInput = useRef(null)
+  
+  const addTodo = () => {
+    var sameValue = false
+    userInput.current.value = ''
+    todos.map(todo => {
+      if (todo.title === task) {
+        sameValue = true
+        alert('same')
+      }
+      return null
+    })
+    if (sameValue || task === '') {
+      return
+    }
+    setTodos([{ title: task, isComplete: false }, ...todos])
+  }
+  const deleteTodo = () => {
+    //settodos start
+    if (todoDelete && !todoDelete.isComplete) {
+      setShowConfirm(true)
+    } else {
+      confirmDelete(todoDelete)
+    }
+
+  }
+  const confirmDelete = () => {
+    setTodos(todos.filter(ele => {
+      if (ele === todoDelete) {
+        console.log(ele)
+        return false
+      } else return true
+    }))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='user-input'>
+        <div className='task-input'>
+          <input type='text' ref={userInput} placeholder='Set a ToDo' onChange={(e) => setTask(e.target.value)} />
+        </div>
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <div className='todo-container'>
+        <ul className='todo-list'>
+          {todos.map((todo) => <li className='todo' key={todo.title}>
+            <span>{todo.title}</span>
+            <button onClick={(e) => {
+              todo.isComplete = true
+              e.target.parentElement.classList.toggle('slashed-text')
+            }
+            }>Done</button>
+            <button onClick={() => { setTodoDelete(todo); deleteTodo() }}>Delete</button>
+          </li>)}
+        </ul>
+      </div>
+      {
+        showConfirm ? <>
+          <div className='confirmDelPop'>
+            <span>Are you sure to delete?</span>
+            <button onClick={() => { confirmDelete(); setShowConfirm(false) }}>Yes</button>
+            <button onClick={() => setShowConfirm(false)}>Cancel</button>
+          </div>
+          <div className='overlay'>
+          </div>
+        </> : null
+      }
+
+
     </div>
   );
 }
